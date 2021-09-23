139b42d9f14818713c175ae8316e40671bd0498f
Kripu77 <84505567+Kripu77@users.noreply.github.com>
2021-09-23 17:50:48 +1000

    stepwise comments

--- src/tutorial/1-useState/setup/1-error-example.js
+++ src/tutorial/1-useState/setup/1-error-example.js
@@ -1,11 +1,11 @@
 import React, {useState} from 'react';
 
 const ErrorExample = () => {
-
+console.log(useState);
 
   return (<div className="container"> 
   
-  
+
 <h1> Click the button to generate new title</h1>
 
   
--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 
 const UseStateBasics = () => {
+ //first index is the value itself and second is the one which refers to the method which will control it.
   return <h2>useState basic example</h2>;
 };
 
