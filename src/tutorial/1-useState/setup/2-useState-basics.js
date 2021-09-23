75066819156587a02170bcb2dbb4fb078e5cadfe
Kripu77 <84505567+Kripu77@users.noreply.github.com>
2021-09-23 18:12:36 +1000

    comments + code update

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,11 +1,25 @@
 import React, { useState } from 'react';
 
 
-const UseStateBasics = () => {
+const UseStateBasics = ({title="Kripu Khadka"}) => {
  //first index is the value itself and second is the one which refers to the method which will control it.
- const [text, setText] = useState("Hi my name is Kripu Khadka");
-  return (  <React.Fragment> 
-  <h2>{text}</h2>
-  </React.Fragment>
-  )};
+ const[text, setText]= useState("Hi my name is "+title)
+ const handleBtnClick = ()=>{
+
+//to check or add the toggle functionality use the if else loop 
+if(text==="Hi my name is Kripu Khadka"){
+setText (`Hi my name is ${title="Asim Bastola"}`);
+}
+else{
+  setText(`Hi my name is ${title}`); //we have used both props and state to understand this
+}
+ }
+ return(
+   <React.Fragment>
+<h1> {text}</h1>
+<button className="btn" onClick={handleBtnClick}> Click me</button>
+   </React.Fragment>
+ );
+
+  };
 export default UseStateBasics;
