2012d0669d706f12b8194c6dd2c7cbafade29412
Kripu77 <84505567+Kripu77@users.noreply.github.com>
2021-09-23 17:22:56 +1000

    starter files updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import React from 'react'
+import React from 'react';
+import ErrorExample from './tutorial/1-useState/setup/1-error-example';
+
 function App() {
   return (
     <div className='container'>
+      <ErrorExample/>
       <h2>Advanced Tutorial</h2>
     </div>
   )
