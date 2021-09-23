0732c263467dddc6a85c2da720d37cef15739d08
Kripu77 <84505567+Kripu77@users.noreply.github.com>
2021-09-23 17:53:52 +1000

    usestate basics

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
 
+
 const UseStateBasics = () => {
  //first index is the value itself and second is the one which refers to the method which will control it.
-  return <h2>useState basic example</h2>;
-};
-
+ const [text, setText] = useState("Hi my name is Kripu Khadka");
+  return (  <React.Fragment> 
+  <h2>{text}</h2>
+  </React.Fragment>
+  )};
 export default UseStateBasics;
