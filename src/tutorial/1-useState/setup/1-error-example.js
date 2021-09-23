47be1127bddc32872d1ea75de0c05fc0367e7c54
Kripu77 <84505567+Kripu77@users.noreply.github.com>
2021-09-23 17:28:05 +1000

    error example update

--- src/tutorial/1-useState/setup/1-error-example.js
+++ src/tutorial/1-useState/setup/1-error-example.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
 const ErrorExample = () => {
-  return (<div className="container"> <h2>useState error example</h2></div>);
+
+  const btnClick =()=>{
+    document.getElementById('changing').innerHTML="changed";
+  }
+  return (<div className="container"> 
+  
+  
+<h1> Click the button to generate new title</h1>
+<h2 id="changing"> Hey</h2>
+<button className="btn" onClick={btnClick}> Click me</button>
+  
+  
+  </div>);
 };
 
 export default ErrorExample;
