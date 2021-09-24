51cb2e95d338a02fbb6dd7007d6c88ebf1c8e879
Kripu77 <84505567+Kripu77@users.noreply.github.com>
2021-09-24 14:51:49 +1000

    update

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -2,34 +2,34 @@ import React,{useState} from 'react';
 import {data} from "../../../data.js";
 
 const UseStateArray = () => {
-  const [people, setPeople] = useState(data);
-const removeSelection = (id) =>{
-  let newPeople = people.filter((person)=>{
-     return person.id !== id
-   
-  })
-   setPeople(newPeople)
-}
-
-return <> 
-{
-
-  people.map((value)=>{
-    const{id, name} = value;
-    console.log(value);
- 
-return(
-      <div className="item" key={id}>  
-    <h4> {name}</h4> 
-    <button onClick= {()=>{return removeSelection(id)}}> remove</button>
-    </div>)
-
   
+  const [ people, setPeople] = useState(data);
+  const removeList = (xx)=>{
+    
+  let newpeople = people.filter((value)=>{
+    return value.id !== xx;
   })
+  setPeople(newpeople);
+  }
 
-}
-<button className="btn" onClick={()=>{setPeople([])}}> remove</button>
-</>
+
+  return <> 
+  {
+    people.map((value)=>{
+const {id, name} = value;
+return (
+<div className="item" key={id}>
+  <h4> {name}</h4>
+  <button onClick={()=>{removeList(id)}}>  remove
+    </button> </div>
+
+
+)
+
+    })
+  }
+  <button  class="btn" onClick={()=>{setPeople([])}}> Remove</button>
+  </>
 };
 
 export default UseStateArray;
