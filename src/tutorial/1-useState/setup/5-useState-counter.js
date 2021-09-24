29d18645a768ca642e24a37d64932ee4352f9592
Kripu77 <84505567+Kripu77@users.noreply.github.com>
2021-09-24 19:56:10 +1000

    setTime Out function

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -1,20 +1,24 @@
 import React, { useState } from 'react';
 
 const UseStateCounter = () => {
-  const [count, setCount] = useState(0)
-  return(
-    <> 
-    
-    <section style={{margin:"4rem 0"}}>
-<h2> regular counter</h2>
-<h1> {count}</h1>
-<button className="btn" onClick={()=>{setCount(count+1)}}> Increment</button>
-<button className="btn" onClick ={()=>{setCount(count-1)}}> Decrement</button>
+// settime out is async hence the function  inside it is taken out form the normal flow. That is why in react in order to solve the problem of not keeping inrecord of the times the user clicked the button despite the delay set. We store the count in a prevCount or prevState argument and then add it hence it will always react to all the times the user clicked the button even if its a async funcrtion
+  const[count, setCount] =useState(0);
+return(<>
+<h1> Async counter</h1>
+<h2> Total Count: {count}</h2>
+<button className="btn" onClick={()=>{
+  setTimeout(()=>{
+    setCount((prevCount)=> prevCount+1)
+  }, 2000)
+}}> IncreMENT</button>
+<button className="btn" onClick={()=>{
+  setTimeout(()=>{
+    setCount((prevCount)=> prevCount-1)
+  }, 2000)
+}}> Decrement</button>
 <button className="btn" onClick={()=>{setCount(0)}}> Reset</button>
 
-    </section>
-    </>
-  )
+</>)
 };
 
 export default UseStateCounter;
