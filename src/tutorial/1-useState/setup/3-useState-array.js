9f7eec69fcc6e765dc29f4fe1d45e2ee3a5302c0
Kripu77 <84505567+Kripu77@users.noreply.github.com>
2021-09-24 14:58:51 +1000

    remove method appllied using filter

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -2,34 +2,31 @@ import React,{useState} from 'react';
 import {data} from "../../../data.js";
 
 const UseStateArray = () => {
-  
-  const [ people, setPeople] = useState(data);
-  const removeList = (xx)=>{
-    
-  let newpeople = people.filter((value)=>{
-    return value.id !== xx;
-  })
-  setPeople(newpeople);
+
+  const[people, setPeople] = useState(data);
+  const removeList = (id)=>{
+const newPeople = people.filter((value)=>{
+  return value.id !== id;
+})
+setPeople(newPeople)
   }
-
-
-  return <> 
+  return <>
   {
     people.map((value)=>{
 const {id, name} = value;
-return (
-<div className="item" key={id}>
-  <h4> {name}</h4>
-  <button onClick={()=>{removeList(id)}}>  remove
-    </button> </div>
-
-
+return(
+  <div className="item" key={id}>
+    <h1> {name}</h1>
+    <button onClick={()=>{removeList(id)}}> remove</button>
+  </div>
 )
 
     })
+    
   }
-  <button  class="btn" onClick={()=>{setPeople([])}}> Remove</button>
+  <button className="btn"  onClick={()=>{setPeople([])}}> Rmeove</button>
+  
   </>
-};
-
+ 
+}
 export default UseStateArray;
