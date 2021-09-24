25f98bc1b32bbe36e6ac56e9429a630eb2da320a
Kripu77 <84505567+Kripu77@users.noreply.github.com>
2021-09-24 14:07:20 +1000

    changes

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -2,22 +2,6 @@ import React,{useState} from 'react';
 import {data} from "../../../data.js";
 
 const UseStateArray = () => {
-  const [person, setPerson] = useState(data);
-
-return <> 
-{
- person.map((value)=>{
-  const {id, name} = value;
-  return(
-<div key={id}className="item">
-<h1> {name}</h1>
-
-</div>)
- })
-}
-
- </>
-
 
 };
 
