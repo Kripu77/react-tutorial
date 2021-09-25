b9d4d1789b53c951c57abae4afb299b76ab9845a
Kripu77 <84505567+Kripu77@users.noreply.github.com>
2021-09-25 22:19:39 +1000

    both the button made reactive

--- src/tutorial/1-useState/final/5-useState-counter.js
+++ src/tutorial/1-useState/final/5-useState-counter.js
@@ -7,8 +7,20 @@ const[count, setCount] = useState(0);
     <>
     <h1> Welcome to my simple counter</h1>\
     <h2> The count right now is {count}</h2>
-<button className="btn"  onClick={()=>{setCount(count+1)}}> Increment Count</button>
-<button className="btn"  onClick={()=>{setCount(count-1)}}> Decrement Count</button>  
+<button className="btn"  onClick={()=>{
+ setTimeout(()=>{
+   setCount((prevState)=>{
+     return prevState+1
+   })
+ }, 2000)
+ }}> Increment Count</button>
+<button className="btn"  onClick={()=>{
+  setTimeout(()=>{
+    setCount((prevState)=>{
+      return prevState-1
+    })
+  }, 2000)
+  }}> Decrement Count</button>  
 <button className="btn"  onClick={()=>{setCount(0)}}> Reset Count</button>    
     </>
   )
