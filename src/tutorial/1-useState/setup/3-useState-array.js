81ec38d1251c9cf0cb6eda448a6d4784d78486cb
Kripu77 <84505567+Kripu77@users.noreply.github.com>
2021-09-30 06:51:35 +1000

    update on the dependencies

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -1,27 +1,8 @@
 import React,{useState} from 'react';
-import {data} from "../../../data"
+import {data} from "../../../data.js"
 
 const UseStateArray = () => {
- const [users, setusers] = useState(data);
- //the common factor to understand is x is just afunction parameter and while we attach it to onClick we are actually taking the id of the person as an argumement
- const removeList =(x)=>{
-setusers(users.filter((value)=>{
-  return value.id !== x;
-}))
- }
-return <>
-{users.map((value)=>{
-  const{id,name}=value;
-return(
-  <div className="item" key={id}> 
-  <h1> {name}</h1>
-  {/* the id is just a function invokation, in the actual function we passed x as an argument to show that they are realted but not stricltly */}
-  <button onClick={()=>{removeList(id)}}> remove</button>
-  
-  </div>)
-})}
-<button onClick={()=>{setusers([])}} className="btn"> Remove </button>
-</>
+ 
  
 }
 export default UseStateArray;
