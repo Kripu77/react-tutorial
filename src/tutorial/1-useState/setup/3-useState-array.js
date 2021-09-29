18f682287024596fee2a5c8d76a15a997b7415e6
Kripu77 <84505567+Kripu77@users.noreply.github.com>
2021-09-30 07:09:58 +1000

    final update

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -1,16 +1,25 @@
 import React,{useState} from 'react';
 import {data} from "../../../data.js"
-
+//filter the list and only return those values whose value is not equal to the invoked value when the button is clicked 
 const UseStateArray = () => {
  const [people, setPeople] = useState(data);
+ const removePeople = (id)=>{
+setPeople(people.filter((value)=>{
+    return value.id!= id;
+    //the new value is set once the filtering is done
+
+}))
+ }
  return <> 
+ {/* map over the data that is coming from our JSON file */}
  {
      people.map((value)=>{
 const {id, name} = value;
 return (
 <div key={id} className="item">
     <h1> {name}</h1>
-    <button> remove</button>
+    {/* set the method to an empty array which does the job in removing the list */}
+    <button onClick={()=>removePeople(id)}> remove </button>
      </div>
      )
 
