6b185d45d42d043885dde1754927e32fdd9ed747
Kripu77 <84505567+Kripu77@users.noreply.github.com>
2021-09-30 20:41:56 +1000

    final commit

--- src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -3,11 +3,29 @@ const url = 'https://api.github.com/users/QuincyLarson';
 const MultipleReturns = () => {
   // conditional rendering means to do or return something if certain condition is met run certain component if block condition meets else run the other block component
   const [isLoading, setIsLoading] = useState(true);
-  const[users, setUsers] = useState(false);
-  return <>
+  const[isError, setIsError] = useState(false);
+  const[users, setUsers] = useState([]);
+
+console.log(users)
+  useEffect(()=>{
+    fetch(url)
+    .then((res)=>{ return res.json()})
+    .then((data)=>{
+      const{login}= data;
+      setUsers(login);
+      setIsLoading(false)});
+  },[])
+
+ if(isLoading){
+   return <h1> Loading....</h1>
+  }
+
+ else{
+   return <h1> {users}</h1>
+ }
 
 
-  </>
+
 };
 
 export default MultipleReturns;
