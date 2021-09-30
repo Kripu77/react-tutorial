00ea5dfb1f259fda28761af378cb45c45414da12
Kripu77 <84505567+Kripu77@users.noreply.github.com>
2021-09-30 21:17:32 +1000

    setTime out fn

--- src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -5,6 +5,37 @@ const MultipleReturns = () => {
 
   const [isLoading, setIsLoading] = useState(true); //initally true 
   const[isError, setIsError] =useState(false); //initially false
+  const [users, setUsers] = useState([]);
+
+
+  useEffect(()=>{
+    fetch(url) .then((res)=>{
+      if(res.status>=200 && res.status<299){
+        return res.json();
+      
+      }
+      else{
+        setIsError(true);
+        setIsLoading(false);
+      }
+    }) 
+    .then((data)=>{
+      //to make the process more fancy we can use a set time out function prior to the value  gets to false
+      setUsers(data.login)
+     setTimeout(()=>{setIsLoading(false)}, 2000);
+    })
+    .catch((error)=>{
+      console.log(error)
+    })
+  },[])
+if(isLoading){
+  return <h1> Is Loading.....</h1>
+}
+
+if(isError){
+  return <h1> There is an error please contact adminstrator</h1>
+}
+return <>{users}</>
 }
 
 export default MultipleReturns;
