5305d2a8cb8f968d5d1af078585a0bec5e729697
Kripu77 <84505567+Kripu77@users.noreply.github.com>
2021-09-30 14:40:32 +1000

    final commit to to clean up fucntion

--- src/tutorial/2-useEffect/final/2-useEffect-cleanup.js
+++ src/tutorial/2-useEffect/final/2-useEffect-cleanup.js
@@ -4,27 +4,8 @@ import React, { useState, useEffect } from 'react';
 // second argument
 
 const UseEffectCleanup = () => {
-  const [size, setSize] = useState(window.innerWidth);
-
-  const checkSize = () => {
-    setSize(window.innerWidth);
-  };
-
-  useEffect(() => {
-    console.log('useEffect');
-    window.addEventListener('resize', checkSize);
-    return () => {
-      console.log('cleanup');
-      window.removeEventListener('resize', checkSize);
-    };
-  }, []);
-  console.log('render');
-  return (
-    <>
-      <h1>window</h1>
-      <h2>{size} PX</h2>
-    </>
-  );
+ return<> 
+ </>
 };
 
 export default UseEffectCleanup;
--- src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -4,15 +4,29 @@ import React, { useState, useEffect } from 'react';
 // second argument
 
 const UseEffectCleanup = () => {
-  const [ width, setWidth ] = useState(window.innerWidth);
-  console.log(width);
-return <> 
-
-
-</>
-
-
-
+let [size, setSize] = useState(window.innerWidth);
+const checkSize = ()=>{
+  setSize(window.innerWidth);
+}
+ if(size>1000){
+    setSize("i'm home");
+    document.title="the size is perfect"
+  }
+  else{
+    document.title="React App"
+  }
+  useEffect(()=>{
+window.addEventListener('resize', checkSize)
+console.log('This is how many timesx use effect is being invoked')
+  
+//clean up function, it is a good practice to set up a clean up function
+return ()=>{
+    window.removeEventListener('resize', checkSize);
+  }
+//  how weever we can set the second argument which will only render the side effect once but, there are scenarios where we have to use clean up fn for preventing from various mess
+  })
+  return <> 
+  {size}</>
 }
 
 
