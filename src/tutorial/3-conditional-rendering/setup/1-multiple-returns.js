47e30fdc07ebd73cc5e073cd18481391bd0b22c4
Kripu77 <84505567+Kripu77@users.noreply.github.com>
2021-09-30 20:52:11 +1000

    error handling

--- src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -2,30 +2,28 @@ import React, { useState, useEffect } from 'react';
 const url = 'https://api.github.com/users/QuincyLarson';
 const MultipleReturns = () => {
   // conditional rendering means to do or return something if certain condition is met run certain component if block condition meets else run the other block component
-  const [isLoading, setIsLoading] = useState(true);
-  const[isError, setIsError] = useState(false);
-  const[users, setUsers] = useState([]);
+const [isLoading, setIsLoading] = useState(true);
+const [isError, setIsError] = useState(false);
+const [users, setUsers] = useState([]);
 
-console.log(users)
-  useEffect(()=>{
-    fetch(url)
-    .then((res)=>{ return res.json()})
-    .then((data)=>{
-      const{login}= data;
-      setUsers(login);
-      setIsLoading(false)});
-  },[])
-
- if(isLoading){
-   return <h1> Loading....</h1>
-  }
-
- else{
-   return <h1> {users}</h1>
- }
-
-
-
-};
+useEffect(()=>{
+  fetch(url) .then((res)=>{ return res.json()}) 
+  .then((data)=>{
+    setUsers(data.login);
+    setIsLoading(false);
+  }) //incase of any error
+  .catch((error)=>{
+    console.log(error)
+    setUsers("problem")
+  })
+},[] 
+) //till the document has not finished loading the loading... logo will appear. Once the loading is completed and the data fecthing process is done the setIsLoading is set to false which renders the users component.
+if(isLoading){
+  return <h1> Loading....</h1>
+}
+else{
+  return <h2> {users}</h2>
+}
+}
 
 export default MultipleReturns;
