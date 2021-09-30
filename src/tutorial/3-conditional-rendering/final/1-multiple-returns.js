d8a05661c516bff4481d8b55dc8b6bfa72e4d82f
Kripu77 <84505567+Kripu77@users.noreply.github.com>
2021-10-01 09:53:59 +1000

    multiple return and filter

--- src/tutorial/3-conditional-rendering/final/1-multiple-returns.js
+++ src/tutorial/3-conditional-rendering/final/1-multiple-returns.js
@@ -4,7 +4,13 @@ const MultipleReturns = () => {
   const [isLoading, setIsLaoding] = useState(true);
   const [isError, setIsError] = useState(false);
   const[users, setUsers] = useState([]);
-  console.log(users)
+ 
+  //filtering and setting the users whose value is not equal to the invoked value in the function
+  const removedList = (id)=>{
+    setUsers(users.filter((value)=>{
+      return value.id != id;
+    }))
+  }
   useEffect(()=>{
 
     //if response doesnot fall under the criteria then the error message will trigger
@@ -26,21 +32,28 @@ const MultipleReturns = () => {
     })
   },[])
 
- 
+ //initial state
   if(isLoading){
     return <h1> Loading....</h1>
   }
+  //when error
   if(isError){
     return<h1> There is an error please reveiw your code</h1>
   }
+  //when none of the condition are met and data is successfully fetched 
   return <>
   {users.map((value)=>{
     const {id, login}= value;
     return <div className="item" key={id}>
       <h1> {login}</h1>
+      <button onClick={()=>{ removedList(id)}}> remove</button>
 
     </div>
   })}
+{/* onclick the users is set to an empty array removing the list of all users */}
+  <buton onClick={()=>
+    setUsers([])
+  } className="btn"> Clear all</buton>
   </>
 }
  
