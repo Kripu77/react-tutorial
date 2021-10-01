13a6474d433c6b2f118cf952c987053c47fa4e35
Kripu77 <84505567+Kripu77@users.noreply.github.com>
2021-10-01 11:08:34 +1000

    changes

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -3,19 +3,17 @@ import React, { useState } from 'react';
 // ternary operator
 
 const ShortCircuit = () => {
-const[ text, setText] = useState('');
-const[isError, setIsError] = useState(false);
-//if first value is falsy then in the case of OR operator it returns the second value
-  const firstValue = text || 'hello world';
-  //if the first value is flasy in AND operator it returns the 1st value.
-
-  const secondValue = text && 'hello world';
-  // in summary these two are exactly opposite to each other and commonly used to setup expression in JSX
+  const[text, setText] = useState('');
+  const[isError, setIsError] = useState(false);
   return <>
-<h1> {text||"The text is falsy"}</h1>
-<h1> {isError && "ERROR 404"}</h1>
-  <button className="btn" onClick={()=>{setIsError(!isError)}}> toogle Error</button>
-   </> 
+  {/* the or operator if it is false then it returns the second parameters */}
+  <h1> {isError||"There is no any error"}</h1>
+  {/* the and operator if it is false then it return the 1st paramter */}
+  <h1> {isError&&"Error....404"}</h1>
+  {/* every time we click we toggle the error from its previous state */}
+  <button className="btn" onClick={()=>{setIsError(!isError)}}>click me</button>
+  
+  </>
 };
 
 export default ShortCircuit;
