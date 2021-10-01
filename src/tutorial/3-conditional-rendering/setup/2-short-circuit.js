46657639e26f273c82a7642d92d35d8adab6ae34
Kripu77 <84505567+Kripu77@users.noreply.github.com>
2021-10-01 11:02:54 +1000

    short circuit

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -4,6 +4,7 @@ import React, { useState } from 'react';
 
 const ShortCircuit = () => {
 const[ text, setText] = useState('');
+const[isError, setIsError] = useState(false);
 //if first value is falsy then in the case of OR operator it returns the second value
   const firstValue = text || 'hello world';
   //if the first value is flasy in AND operator it returns the 1st value.
@@ -11,8 +12,9 @@ const[ text, setText] = useState('');
   const secondValue = text && 'hello world';
   // in summary these two are exactly opposite to each other and commonly used to setup expression in JSX
   return <>
-  <h1> {firstValue}</h1>
-  <h1> value: {secondValue}</h1>
+<h1> {text||"The text is falsy"}</h1>
+<h1> {isError && "ERROR 404"}</h1>
+  <button className="btn" onClick={()=>{setIsError(!isError)}}> toogle Error</button>
    </> 
 };
 
