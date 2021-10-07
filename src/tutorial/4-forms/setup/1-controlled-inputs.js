a3d3dc6d5fabda93e75fe83454d289de01382a34
Kripu77 <84505567+Kripu77@users.noreply.github.com>
2021-10-07 17:45:46 +1100

    reset all the files

--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -6,32 +6,44 @@ import React, { useState } from 'react';
 // value, onChange
 
 const ControlledInputs = () => {
-    const[firstName, setFirstName] = useState('')
-     const[email, setEmail] = useState('')
-    const handleSubmit = (e)=>{
-        e.preventDefault();
-        console.log(`${firstName, email}` )
-    }
- return <>
- <article>
-     <form className="form" onSubmit={handleSubmit}>
-         <div className="form-control">
+ 
+return <>
+<article>
+<form>
+<div className="form">
+    <label htmlFor="firstName">
+        First Name
+    </label>
+    <input type="text" name="firstName" id="firstName"/>
 
-             <label htmlFor="firstName">Name:</label>
-             <input type="text" id="firstName" name="firstName" value={firstName} onChange={(e)=> setFirstName(e.target.value)}/>
-         </div>
-          <div className="form-control">
+<br/>
+<br/>
 
-             <label htmlFor="Email">Name:</label>
-             <input type="text" id="email" name="email" value={email} onChange={(e)=>setEmail(e.target.value)}/>
-         </div>
-{/* in react we have two options we can either add onSubmit on the form if we use submit on the button whereas we can also use onclick on the button */}
-<button type="submit"> Add Person?</button>
-     </form>
+    <label htmlFor="lastName">
+        Last Name
+    </label>
+    <input type="text" name="lastName" id="lastName"/>
+    
+<br/>
+<br/>
+
+    <label htmlFor="email">
+      Email
+    </label>
+    <input type="text" name="email" id="email"/>
 
 
- </article>
- </>
+</div>
+<button> Add user?</button>
+
+
+
+
+</form>
+
+</article>
+
+</>
 };
 
 export default ControlledInputs;
