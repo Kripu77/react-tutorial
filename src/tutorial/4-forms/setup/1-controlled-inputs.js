1c1a63ea136590e663466ede178af864f96b01e6
Kripu77 <84505567+Kripu77@users.noreply.github.com>
2021-10-07 19:12:19 +1100

    Controlled input

--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -17,11 +17,20 @@ const ControlledInputs = () => {
     const handleClick =(e)=>{
         e.preventDefault();
         //to add mapping fucntionality we first convert the value into an object then an array
-      const person = {firstName, lastName}
+      //we only want to insert the values when first name and lastname are not empty
+      if(firstName&& lastName){
+        const person = {firstName, lastName}
       const allPeople  = [person]
      
       //use setmethod to populate people
-    }
+      //we donot want to insert empty value hence we do truthy and flasy check using and operator
+    
+     setPeople((prevState)=>[...prevState, allPeople])
+      console.log(people)
+
+      //once the data is done with insertion we want to reset the feilds to empty or just clear it
+    setFirstName('')
+setLastName('')}}
 return <article>
    
         {/* on the onsubmit method we just provide a reference we donot invoke it */}
