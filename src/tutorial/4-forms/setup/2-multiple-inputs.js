1519ce8e2b96111a62e916c019f33810e886b684
Kripu77 <84505567+Kripu77@users.noreply.github.com>
2021-10-08 10:48:46 +1100

    reset all the files

--- src/tutorial/4-forms/setup/2-multiple-inputs.js
+++ src/tutorial/4-forms/setup/2-multiple-inputs.js
@@ -7,41 +7,42 @@ import React, { useState } from 'react';
 // dynamic object keys
 
 const ControlledInputs = () => {
-  const[person, setPerson] = useState({firstName:'', lastName:'', email:''});
-  const[totalPeople, setPeople] = useState([])
-  const handleSubmit= (e)=>{
-    e.preventDefault();
-
-  }
-
-  const handleClick = (e)=>{
-    const name = e.target.name
-    const value = e.target.value
-    console.log(name, value)
-  }
+ 
 return <>
-<article>
-<div className="form" onSubmit={handleSubmit}>
-  <form>
-    <label htmlFor="firstName"> First Name:
-    </label>
-    <input type="text" name="firstName" id="firstName" value={person.firstName} onChange = {handleClick}></input>
-    <br/>
-    <br/>
-     <label htmlFor="lastName"> Last Name:
-    </label>
-    <input type="text" name="lastName" id="lastName" value={person.lastName} onChange= {handleClick}></input>
-    <br/>
-    <br/>
-     <label htmlFor="email"> email:
-    </label>
-    <input type="text" name="email" id="email" value={person.email} onChange={handleClick}></input>
-    <br/>
-    <button type="submit">Submit?</button>
-  </form>
+
+<div className="form">
+<form>
+
+<label htmlFor="fName">
+  First Name:
+</label>
+
+<input type="text" id="fName" name="fName" ></input>
+<br/>
+<br/>
+
+<label htmlFor="LName">
+  Last Name
+</label>
+
+<input type="text" id="LName" name="LName" ></input>
+<br/>
+<br/>
+
+
+<label htmlFor="email">
+Email:
+</label>
+
+<input type="text" id="email" name="email" ></input>
+<br/>
+<br/>
+<button type="submit"> Add user</button>
+</form>
+
 
 </div>
-</article>
+
 </>
 };
 
