09f09c8e5389134d033af22cc348d5d363a42e11
Kripu77 <84505567+Kripu77@users.noreply.github.com>
2021-10-07 17:29:37 +1100

    updates

--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -6,8 +6,26 @@ import React, { useState } from 'react';
 // value, onChange
 
 const ControlledInputs = () => {
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+        console.log("Hello you =have cliked me")
+    }
  return <>
  <article>
+     <form className="form" onSubmit={handleSubmit}>
+         <div className="form-control">
+
+             <label htmlFor="firstName">Name:</label>
+             <input type="text" id="firstName" name="firstName"/>
+         </div>
+          <div className="form-control">
+
+             <label htmlFor="Email">Name:</label>
+             <input type="text" id="email" name="email"/>
+         </div>
+{/* in react we have two options we can either add onSubmit on the form if we use submit on the button whereas we can also use onclick on the button */}
+<button type="submit"> Add Person?</button>
+     </form>
 
 
  </article>
