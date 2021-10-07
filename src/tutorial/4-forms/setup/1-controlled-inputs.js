f0487302a41806b920b5912b2a196f59cd9a79bf
Kripu77 <84505567+Kripu77@users.noreply.github.com>
2021-10-07 18:33:35 +1100

    truthy

--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -6,21 +6,38 @@ import React, { useState } from 'react';
 // value, onChange
 
 const ControlledInputs = () => {
+    // store the input value in state
+    const[firstName, setFirstName] = useState('');
+    const[lastName, setLastName] = useState('');
+    const[email, setEmail] = useState('');
+    const[people, setPeople] = useState([]);
  
-    // prevent default behaviour from happening that is page refresh
+    // prevent default behaviour from happening that is page refresh which submit button is clicked
     const handleClick =  (e)=>{
 e.preventDefault();
+if(firstName && lastName && email){
+    const person = {firstName, lastName, email}
+        console.log(person)
+   const allPerson = [...people,person];
+   console.log(allPerson)
+   setPeople(allPerson)
+
+}
+
     }
 
 
 return <>
 <article>
+    {/* using the onsubmit method */}
+    {/* while we pass on a function to check something we just refer to the function rather than invoking it */}
 <form onSubmit={handleClick}>
 <div className="form">
     <label htmlFor="firstName">
         First Name
     </label>
-    <input type="text" name="firstName" id="firstName"/>
+    {/* once we have the value we need to have the onChange property if not we are only following readonly approach */}
+    <input type="text" name="firstName" id="firstName" value={firstName} onChange={(e)=>setFirstName(e.target.value)}/>
 
 <br/>
 <br/>
@@ -28,20 +45,31 @@ return <>
     <label htmlFor="lastName">
         Last Name
     </label>
-    <input type="text" name="lastName" id="lastName"/>
+    <input type="text" name="lastName" id="lastName" value={lastName} onChange={(e)=>setLastName(e.target.value)}/>
     
 <br/>
 <br/>
 
-    <label htmlFor="email">
+    <label htmlFor="email" >
       Email
     </label>
-    <input type="text" name="email" id="email"/>
+    <input type="text" name="email" id="email" value={email} onChange={(e)=>setEmail(e.target.value)}/>
     <br/>
 <button type="submit"> Add user?</button>
 
 </div>
 
+<div className="form">
+    {people.map((value, index)=>{
+        const[firstName, lastName, email] = value;
+        return <div>
+            <h1 style={{color:"black"}}> {firstName}</h1>
+
+            </div>
+
+    })}
+
+</div>
 
 
 
