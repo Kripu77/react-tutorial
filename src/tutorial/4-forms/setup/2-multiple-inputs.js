842cfd29132468646ad0ead31a23310b9bd15cab
Kripu77 <84505567+Kripu77@users.noreply.github.com>
2021-10-08 11:30:55 +1100

    input form is ready

--- src/tutorial/4-forms/setup/2-multiple-inputs.js
+++ src/tutorial/4-forms/setup/2-multiple-inputs.js
@@ -10,9 +10,42 @@ const ControlledInputs = () => {
   //while setting up the initial empty state we have to ensure the name matches to the input form 
 
   const [details, setDetails] = useState({firstName:'', lastName:"", email:""})
+  
+  //fn to prevent default behaviour of refresh
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+   
+
+  }
+
+  //fn to handle change
+
+  const handleChange = (e)=>{
+const name= e.target.name;
+const value = e.target.value;
+// spread the previous value and then set the name which is in the form equal to the value that we are typing dynamically
+setDetails({...details,[name]:value})
+  }
 return <>
 
+<div className="form" onSubmit={handleSubmit}>
+  <form>
+<label htmlFor="firstName">First Name:</label>
+<input type="text" id="firstName" name="firstName" value={details.firstName} onChange={handleChange}></input>
+<br/>
+<br/>
+<label htmlFor="lastName"> Last Name:</label>
+<input type="text" id="lastName" name="lastName" value={details.lastName} onChange={handleChange}></input>
 
+<br/>
+<br/>
+<label htmlFor="email"> Email:</label>
+<input type="text" id="email" name="email" value={details.email} onChange={handleChange}></input>
+<br/>
+<button type="onSumbit"> Submit</button>
+  </form>
+
+</div>
 </>
 };
 
