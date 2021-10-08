da586c35e8c66cc9c0f8cc75e599d985695b8fff
Kripu77 <84505567+Kripu77@users.noreply.github.com>
2021-10-08 11:21:02 +1100

    changes in input state

--- src/tutorial/4-forms/setup/2-multiple-inputs.js
+++ src/tutorial/4-forms/setup/2-multiple-inputs.js
@@ -7,55 +7,11 @@ import React, { useState } from 'react';
 // dynamic object keys
 
 const ControlledInputs = () => {
- const[ details, setDetails] = useState({firstName:"", lastName:"",email:""})
-const [people, setPeople] = useState([])
-const handleSubmit = (e)=>{
-e.preventDefault();
-}
+  //while setting up the initial empty state we have to ensure the name matches to the input form 
 
-//fn to handle change
-const handleChange = (e)=>{
-  const name=e.target.name;
-const value= e.target.value;
-
-console.log(name, value)
-setDetails({...details, [name]:value})
-}
-
+  const [details, setDetails] = useState({firstName:'', lastName:"", email:""})
 return <>
 
-<div className="form">
-<form onSubmit={handleSubmit}>
-
-<label htmlFor="fName">
-  First Name:
-</label>
-
-<input type="text" id="fName" name="firstName" value={details.firstName} onChange={handleChange}></input>
-<br/>
-<br/>
-
-<label htmlFor="LName" >
-  Last Name
-</label>
-
-<input type="text" id="LName" name="lastName" value={details.lastName}  onChange={handleChange}></input>
-<br/>
-<br/>
-
-
-<label htmlFor="email">
-Email:
-</label>
-
-<input type="text" id="email" name="email" value={details.email}  onChange={handleChange}></input>
-<br/>
-<br/>
-<button type="submit"> Add user</button>
-</form>
-
-
-</div>
 
 </>
 };
