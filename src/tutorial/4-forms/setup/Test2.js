024ca64af0bea2a50627d3b3844d609433229d1a
Kripu77 <84505567+Kripu77@users.noreply.github.com>
2021-10-13 11:00:06 +1100

    update

--- src/tutorial/4-forms/setup/Test2.js
+++ src/tutorial/4-forms/setup/Test2.js
@@ -2,12 +2,20 @@ import React,{useState} from 'react'
 
 const Testt = () => {
     const[userDetails, setUserDetails] = useState({fName:"", lName:"", email:""})
-
+const [finalDetails, setFinalDetails] = useState([])
     const handleSubmit = (e)=>{
         e.preventDefault();
     }
     //the handle change function
-    const handleChange = ()=>{
+    const handleChange = (e)=>{
+const name =  e.target.name;
+const value=  e.target.value;
+
+const {fName, lName, email} = userDetails;
+if(fName && lName &&email){
+    const details = [userDetails]
+    console.log(details);
+}
 
     }
     return (
