10a923f342295e8ff0bb880b4c9912cdf0bff017
Kripu77 <84505567+Kripu77@users.noreply.github.com>
2021-10-19 17:24:46 +1100

    update

--- src/tutorial/5-useRef/setup/1-useRef-basics.js
+++ src/tutorial/5-useRef/setup/1-useRef-basics.js
@@ -5,27 +5,34 @@ import React, { useEffect, useRef } from 'react';
 // target DOM nodes/elements
 
 const UseRefBasics = () => {
-  const inputRef = useRef('')
+const inputRef =  useRef('')
+const divRef = useRef('')
+  // handleSubmit fn
+  const handleSubmit = (e)=>{
+e.preventDefault();
+// if we console log then we get access to a current property from the ref hook
+console.log(inputRef.current.value)
+console.log(divRef)
+console.log(divRef.current)
+  }
+
+  //use effect to check the focus and run only once, hence no need to have any dependency if it will only trigger once
+  useEffect(()=>{
+inputRef.current.focus();
+
+  })
+return <>
 
 
- //to handlethe submission
+<form className='form' onSubmit={handleSubmit}>
+  <input type='text' ref={inputRef}></input>
+  <button className='submit' >Submit</button>
+</form>
 
- const handleClick = (e)=>{
-   e.preventDefault();
-  //  if we console.log our ref property then we get access to current object from which we can get access to the value that we type on the input feild
- console.log(inputRef)
- console.log(inputRef.current.value)
-
- }
- return <>
-  
-  <form className='form' onSubmit={handleClick}>
-    <input type='text' ref={inputRef}></input>
-    <button type="Submit" > Submit</button>
-
-  </form>
-  
-  </>
+<div ref={divRef}> 
+  <h1> hello</h1>
+</div>
+</>
 };
 
 export default UseRefBasics;
