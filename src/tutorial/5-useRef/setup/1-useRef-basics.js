1fdb56dd839a73b849552e08b438feaa19ce605f
Kripu77 <84505567+Kripu77@users.noreply.github.com>
2021-10-19 16:59:41 +1100

    UseRef hook setup

--- src/tutorial/5-useRef/setup/1-useRef-basics.js
+++ src/tutorial/5-useRef/setup/1-useRef-basics.js
@@ -5,7 +5,25 @@ import React, { useEffect, useRef } from 'react';
 // target DOM nodes/elements
 
 const UseRefBasics = () => {
-  return <h2>useRef</h2>;
+  const inputRef = useRef('')
+
+
+ //to handlethe submission
+
+ const handleClick = (e)=>{
+   e.preventDefault();
+   console.log(inputRef.current.value)
+
+ }
+ return <>
+  
+  <form className='form' onSubmit={handleClick}>
+    <input type='text' ref={inputRef}></input>
+    <button type="Submit" > Submit</button>
+
+  </form>
+  
+  </>
 };
 
 export default UseRefBasics;
