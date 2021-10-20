f9998a56ff9a26b4e9cd7484c9008491754ce24f
Kripu77 <84505567+Kripu77@users.noreply.github.com>
2021-10-20 19:27:14 +1100

    Reducer simple demo completed

--- src/tutorial/6-useReducer/setup/Reducer.js
+++ src/tutorial/6-useReducer/setup/Reducer.js
@@ -1,9 +1,36 @@
-import React,{useEffect} from 'react'
+import React,{useReducer} from 'react'
+//the reducer function
+//action is the argument which is resposonsible in controlling the current state
+//action provides reducer the information on what task to perform
+const reducer = (currentState, action)=>{
 
+
+    if(action === 'increment'){
+        return currentState+1
+    }
+    else if(action === 'decrement'){
+        return currentState-1
+    }
+    else if(action ==='reset'){
+        return initialState
+    }
+   
+
+}
+const initialState = 0
 const Reducer = () => {
+//using dispatch allows us to execute certain code which is corresponding to particular action
+
+   const [state, dispatch] = useReducer(reducer, initialState)
     return (
         <div>
-            
+            <h1>{state}</h1>
+          <button onClick={()=>dispatch('increment')}>Increment</button>  
+           <button onClick={()=>dispatch('decrement')}>Decrement</button>  
+ 
+         <button onClick={()=>dispatch('reset')}>Reset</button>  
+      
+
         </div>
     )
 }
