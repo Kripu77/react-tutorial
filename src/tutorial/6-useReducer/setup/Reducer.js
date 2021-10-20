c2b2935241340bf028b0d0daeb52c4a573892a53
Kripu77 <84505567+Kripu77@users.noreply.github.com>
2021-10-20 19:19:02 +1100

    Reset use reducer

--- src/tutorial/6-useReducer/setup/Reducer.js
+++ src/tutorial/6-useReducer/setup/Reducer.js
@@ -1,34 +1,9 @@
-import React, {useReducer} from 'react'
+import React,{useEffect} from 'react'
 
-
-const reducer= (currentState, action)=>{
-    if(action==='increment'){
-return currentState+1
-    }
-    else if(action==='decrement'){
-        return currentState-1
-    }
-    else if(action==='reset'){
-        return initialState
-    }
-    else{
-        return currentState
-    }
-    }
-
-
-const initialState = 0
 const Reducer = () => {
-
-const [state, dispatch] = useReducer(reducer, initialState)
     return (
         <div>
-            <h1>{state}</h1>
-            <button onClick={()=>dispatch('increment')}>
-                Increment
-            </button>
-            <button onClick={()=>dispatch('decrement')}>Decrement</button>
-            <button onClick={()=>dispatch('reset')}>Reset</button>
+            
         </div>
     )
 }
