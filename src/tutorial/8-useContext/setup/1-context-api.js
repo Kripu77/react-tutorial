92ff9c9950c5669a4eb3fad9e4fb642d956d5e76
Kripu77 <84505567+Kripu77@users.noreply.github.com>
2021-10-22 10:40:24 +1100

    Udpated

--- src/tutorial/8-useContext/setup/1-context-api.js
+++ src/tutorial/8-useContext/setup/1-context-api.js
@@ -1,30 +1,11 @@
-
-
 import React from 'react'
-import ComponentA from './ComponentA';
-import ComponentB from './ComponentB';
-import ComponentC from './ComponentC';
 
-
-export const userContext =  React.createContext();
-export const secondContext = React.createContext();
-
-const ContextAPI= () => {
+const contextAPI = () => {
   return (
     <div>
-
-      <userContext.Provider value={"Kripu"}>
-        <ComponentA/>
-    
-
-      </userContext.Provider>
-      <secondContext.Provider value={"Second Context"}>
-
-        <ComponentB/>
-      </secondContext.Provider>
       
     </div>
   )
 }
 
-export default ContextAPI;
+export default contextAPI
