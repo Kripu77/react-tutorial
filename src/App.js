7d1eb264d014bc3147ee817f7a6f54218527ce4e
Kripu77 <84505567+Kripu77@users.noreply.github.com>
2021-10-25 22:57:58 +1100

    Updated

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Index from './tutorial/10-prop-types/setup';
 import ReducerObject from './tutorial/6-useReducer/setup/ReducerObject';
 import testModal from './tutorial/6-useReducer/setup/testModal';
 import ContextAPI from './tutorial/8-useContext/setup/1-context-api';
@@ -15,7 +16,7 @@ import { useFetch } from './tutorial/9-custom-hooks/setup/2-useFetch';
 function App() {
   return (
     <div className='container'>
-<Example/>
+<Index/>
     </div>
   )
 }
