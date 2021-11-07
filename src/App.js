f06589137de8589bf1a2eea4530b728016abb939
Kripu77 <84505567+Kripu77@users.noreply.github.com>
2021-11-08 10:33:38 +1100

    Clean upfunctionintroduction react routerversion 6
    Pleaseupgrade to react  router  version 6

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import React from 'react';
-import Index from './tutorial/10-prop-types/setup';
+
 import ReactRouterSetup from './tutorial/11-react-router/setup';
-import ReducerObject from './tutorial/6-useReducer/setup/ReducerObject';
-import testModal from './tutorial/6-useReducer/setup/testModal';
-import ContextAPI from './tutorial/8-useContext/setup/1-context-api';
-import Example from './tutorial/9-custom-hooks/setup/1-fetch-example';
-import { useFetch } from './tutorial/9-custom-hooks/setup/2-useFetch';
 
 
 
